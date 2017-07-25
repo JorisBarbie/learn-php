00698c83e6735a5c3b89e8078a2a2009b31c0a78
JorisBarbie <jojoloys@gmail.com>
2017-07-25 16:09:27 +0200

    Les Includes

---
+++ Fonctioninclude.php
@@ -0,0 +1,18 @@
+
+<div id="menu" style="background-color: red; width: 150px ;float;left;"> 
+    <ol>
+     <li><a href="#"> lien</a></li> 
+     <li><a href="#"> lien</a></li> 
+     <li><a href="#"> lien</a></li> 
+     <li><a href="#"> lien</a></li> 
+     <li><a href="#"> lien</a></li> 
+     <li><a href="#"> lien</a></li> 
+    </ol>
+</div>
+
+
+
+
+
+
+
---
+++ include.php
@@ -0,0 +1,14 @@
+<html>
+    <head></head>
+
+    <body>
+    <h1> Meilleur titre pour un site </h1>
+    <?php 
+    include ("Fonctioninclude.php") ;
+    ?>
+    
+    
+    
+    
+    
+    </body> 
